b1f14fb383832e2a8ddfeb7946cace6c20359ac4
CoderTrudy <raeann.clarke@mymona.uwi.edu>
2024-02-28 22:51:05 -0500

    created and edited some files

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
+
+from app import forms 
 
 
 ###
@@ -17,7 +22,31 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form=ContactForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            # Form validation passed, so send the email
+            name = form.name.data
+            email = form.email.data
+            subject = form.subject.data
+            message = form.message.data
 
+            msg = Message(subject,
+                          sender=(name, email),
+                          recipients=["youremail@example.com"])
+            msg.body = message
+            mail.send(msg)
+
+            flash('Your message has been sent!', 'success')
+            return redirect(url_for('home'))
+        else:
+            # Form validation failed, display the contact form with errors
+            flash('Please check your input.', 'danger')
+
+    # If it's a GET request or validation failed, render the contact form
+    return render_template('contact.html', form=form)
 ###
 # The functions below should be applicable to all Flask apps.
 ###
